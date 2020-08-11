1d8f9da740b65dfdf30f43159a8c117d8a4d11d8
nguyenquangbinh <nguyenquangbinh803@gmail.com>
2020-08-11 18:14:09 +0700

    Update get dislocation

--- RobotGM6020.py
+++ RobotGM6020.py
@@ -3,6 +3,7 @@ import time
 from datetime import datetime
 import sys
 from UtilitiesMacroAndConstant import *
+import numpy as np
 
 class RobotControl:
 
@@ -40,6 +41,33 @@ class RobotControl:
         self.write_steer_command('3,' + str(spin_angle))
         self.write_drive_command('3,' + str(spin_angle))
 
+    def read_command(self):
+        '''
+        request robot current position
+        '''
+        self.__robot_serial_drive.write("5;".encode("utf-8"))
+        bytesRead = self.__robot_serial_drive.inWaiting()
+        return self.__robot_serial_drive.read(bytesRead)
+
+    def convert_string_to_int32(self, strData):
+        return (np.int32(strData[0]) << 24) + (np.int32(strData[1]) << 16) + (np.int32(strData[2]) << 8) + (
+            np.int32(strData[3]))
+
+    def get_dislocation(self):
+        raw_data = str(self.read_command(), 'utf-8').split()
+
+        # Check length of receive buffer with start and end frame
+        if (len(raw_data) == LENGTH_OF_DATA_RECEIVE and raw_data[0] == START_FRAME and raw_data[-1] == END_FRAME):
+            receive_data = raw_data[1:-1]
+        else:
+            return []
+
+        distance_front_left = self.convert_string_to_int32(receive_data[1:5])
+        distance_front_right = self.convert_string_to_int32(receive_data[6:10])
+        distance_rear_left = self.convert_string_to_int32(receive_data[11:15])
+        distance_rear_right = self.convert_string_to_int32(receive_data[16:])
+        return distance_front_left, distance_front_right, distance_rear_left, distance_rear_right
+
     def write_steer_command(self, command):
         '''
         Write command to serial
@@ -70,6 +98,7 @@ if __name__ == "__main__":
     while True:
         robot.write_steer_command("q,45,45,-45,-45")
         robot.write_drive_command("n,1,1,1,1")
+        print(robot.get_dislocation())
         time.sleep(0.1)
 
 
